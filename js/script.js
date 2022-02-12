4996ffdf8c3b1f096a3ab3a167983f071077afdc
JaiSinghal02 <joyfuljai02@gmail.com>
2022-02-12 16:53:42 +0530

    Added country search bar

--- js/script.js
+++ js/script.js
@@ -7,38 +7,83 @@ function updatePreferenceCookie(place){
     array.unshift(place)
     localStorage.setItem('preference',JSON.stringify(array))
 }
+function buttonOff(button){
+    button.classList.add('disabled-button')
+    button.value = 'Fetching'
+}
+function buttonOn(button){
+    button.classList.remove('disabled-button')
+    button.value = 'Search'
+}
 async function getCoordinates(location){
-    var data = await (await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${key}`)).json()
-    var data = data[0]
-    const lat = data.lat
-    const lon = data.lon
-    return [lat,lon]
+    try{
+        var data = await (await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${key}`)).json()
+        console.log(data)
+        var data = data[0]
+        const lat = data.lat
+        const lon = data.lon
+        return [lat,lon]
+    }
+    catch(err){
+        console.log(err)
+        return [null,null]
+    }
+}
+async function getCities(country){
+    const body = {country}
+    console.log(body)
+    try{
+        var cities = (await fetch('https://shivammathur.com/countrycity/cities/india')).data
+        return cities
+    }
+    catch(err){
+        console.log(err.data)
+        return null
+    }
 }
 async function getWeatherData(lat,lon){
-    var data = await (await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`)).json()
-    return data
+    try{
+        var data = await (await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`)).json()
+        return data
+    }
+    catch(err){
+        console.log(err)
+    }
 }
 
 async function fetchPlaceWeather(){
     const place = 'london'
     updatePreferenceCookie(place)
     const [lat,lon] = await getCoordinates(place)
-    const data = await getWeatherData(lat,lon)
-    console.log(data)
-    const maxTemp = data.main.temp_max
-    const minTemp = data.main.temp_min
-    const temp = data.main.temp
-    const weather = data.weather[0].description
-    const iconId = data.weather[0].icon
+    if(lat!==null){
+        const data = await getWeatherData(lat,lon)
+        console.log(data)
+        const maxTemp = data.main.temp_max
+        const minTemp = data.main.temp_min
+        const temp = data.main.temp
+        const weather = data.weather[0].description
+        const iconId = data.weather[0].icon
+    }
     console.log(maxTemp,minTemp)
 }
 
-function fetchCountryWeather(){
-    const country = 'Britain'
-    const [lat,lon] = await getCoordinates(country)
-    const data = await getWeatherData(lat,lon)
-    const maxTemp = data.main.temp_max
-    const minTemp = data.main.temp_min
+async function fetchCountryWeather(button){
+    buttonOff(button)
+    const country = document.getElementById('country-input').value
+    const cities = getCities(country)
+    var maxTemp = -1000,minTemp=1000
+    if(cities!==null){
+        for(let i in cities){
+            const [lat,lon] = await getCoordinates(cities[i])
+            if(lat!=null){
+                const data = await getWeatherData(lat,lon)
+                maxTemp = Math.max(maxTemp,data.main.temp_max)
+                minTemp = Math.min(minTemp,data.main.temp_min)
+            }
+        }
+    }
+    
+    buttonOn(button)
 
 }
 
