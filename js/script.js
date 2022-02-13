76358ca2923c0d36765c7be744585e60e1d89fab
JaiSinghal02 <joyfuljai02@gmail.com>
2022-02-13 13:25:26 +0530

    Added city only search and preference tags

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ function updatePreferenceCookie(place){
     array.unshift(place)
     localStorage.setItem('preference',JSON.stringify(array))
 }
+
 function buttonOff(button){
     button.classList.add('disabled-button')
     button.value = 'Fetching'
@@ -15,30 +16,63 @@ function buttonOn(button){
     button.classList.remove('disabled-button')
     button.value = 'Search'
 }
+
+function showPreferenceTags(){
+    const preferenceElementBox = document.getElementsByClassName('preference-box')[0]
+    preferenceElementBox.style.contentVisibility = 'visible'
+    const preferenceTagBox = preferenceElementBox.getElementsByClassName('preference-tags')[0]
+    var preferences = JSON.parse(localStorage.getItem('preference'))
+    if(!preferences){
+        preferences = []
+        preferenceElementBox.style.contentVisibility = 'hidden'
+    }
+    preferenceTagBox.innerHTML=''
+    preferences.forEach((preference)=>{
+        preferenceTagBox.innerHTML +=`
+            <div class="preference-tag">${preference}</div> 
+        `
+
+    })
+}
+
+function showResult (cityName,cityTemp,cityWeatherIcon,cityWeather){
+    const resultBoxElement = document.getElementsByClassName('result-box-div')[0]
+    const resultBoxes = resultBoxElement.getElementsByClassName('result-box')
+    const numberOfResultBoxes = resultBoxes.length
+    console.log('lenght=',numberOfResultBoxes)
+    let id = 0
+    if(numberOfResultBoxes > 0) {
+        id=parseInt(resultBoxes[numberOfResultBoxes-1].id.split("-")[1])+1
+    }
+    if(numberOfResultBoxes >=3){
+        const indexToBeRemoved = (3*(Math.floor(numberOfResultBoxes/3)-1)) + (numberOfResultBoxes%3)
+        resultBoxes[indexToBeRemoved].style.display = 'none'
+    }
+    resultBoxElement.style.contentVisibility = 'visible'
+    resultBoxElement.innerHTML += `
+    <div class="result-box" id="box-${id}">
+        <p class="city-name">${cityName}</p>
+        <p class="city-temp">${cityTemp} <sup id="degree">&#8451;</sup></p>
+        <div class="weather-div">
+            <div class="weather-icon" style="background-image: url(http://openweathermap.org/img/wn/${cityWeatherIcon}@2x.png)"></div>
+            <div class="weather-name">${cityWeather}</div>
+        </div>
+    </div>`
+}
+
 async function getCoordinates(location){
     try{
         var data = await (await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${key}`)).json()
-        console.log(data)
+        // console.log(data)
         var data = data[0]
         const lat = data.lat
         const lon = data.lon
-        return [lat,lon]
+        const name = data.name
+        return [lat,lon,name]
     }
     catch(err){
         console.log(err)
-        return [null,null]
-    }
-}
-async function getCities(country){
-    const body = {country}
-    console.log(body)
-    try{
-        var cities = (await fetch('https://shivammathur.com/countrycity/cities/india')).data
-        return cities
-    }
-    catch(err){
-        console.log(err.data)
-        return null
+        return [null,null,null]
     }
 }
 async function getWeatherData(lat,lon){
@@ -50,42 +84,43 @@ async function getWeatherData(lat,lon){
         console.log(err)
     }
 }
-
-async function fetchPlaceWeather(){
-    const place = 'london'
-    updatePreferenceCookie(place)
-    const [lat,lon] = await getCoordinates(place)
-    if(lat!==null){
-        const data = await getWeatherData(lat,lon)
-        console.log(data)
-        const maxTemp = data.main.temp_max
-        const minTemp = data.main.temp_min
-        const temp = data.main.temp
-        const weather = data.weather[0].description
-        const iconId = data.weather[0].icon
+function checkRepeatedData(cityName){
+    const cities = document.getElementsByClassName('city-name')
+    console.log(cities.length,cities)
+    for(let i=0;i<cities.length;++i){
+        const city = cities[i].innerText.toLowerCase()
+        if(city===cityName.toLowerCase()) return true
     }
-    console.log(maxTemp,minTemp)
+    return false
 }
 
-async function fetchCountryWeather(button){
+
+async function fetchCityWeather(button){
     buttonOff(button)
-    const country = document.getElementById('country-input').value
-    const cities = getCities(country)
-    var maxTemp = -1000,minTemp=1000
-    if(cities!==null){
-        for(let i in cities){
-            const [lat,lon] = await getCoordinates(cities[i])
-            if(lat!=null){
-                const data = await getWeatherData(lat,lon)
-                maxTemp = Math.max(maxTemp,data.main.temp_max)
-                minTemp = Math.min(minTemp,data.main.temp_min)
-            }
+    const city = document.getElementById('city-input').value
+    if(!checkRepeatedData(city)){
+        const [lat,lon,name] = await getCoordinates(city)
+        if(lat!==null){
+            const data = await getWeatherData(lat,lon)
+            // console.log(data)
+            const maxTemp = data.main.temp_max
+            const minTemp = data.main.temp_min
+            const temp = data.main.temp
+            const weather = data.weather[0].description
+            const iconId = data.weather[0].icon
+            updatePreferenceCookie(city)
+            showResult(name,temp,iconId,weather)
+            showPreferenceTags()
+            // console.log(maxTemp,minTemp)
         }
     }
     
     buttonOn(button)
-
 }
 
+
+
+
+window.onload = showPreferenceTags()
 
 // for icon url http://openweathermap.org/img/wn/10d@2x.png
